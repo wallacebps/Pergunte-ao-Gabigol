055cc99707c7e82ee955cd6ee5f622d5735ccc61
wallacebps <wallacebps@hotmail.com>
2023-01-04 20:39:31 -0300

    Script principal adicionado

---
+++ scripts.js
@@ -0,0 +1,37 @@
+var img = document.querySelector("#img-resposta");
+var pergunte = document.querySelector('#pergunte');
+var btn = document.querySelector('#btn-enviar');
+var resp_sim = document.querySelector('#resposta-sim');
+var resp_nao = document.querySelector('#resposta-nao');
+
+pergunte.addEventListener('input',function(event){
+    if (!event.target.value){
+        btn.disabled = true
+    }else {
+        btn.disabled = false
+    }
+})
+
+function perguntando() {
+    const random = Math.floor(Math.random() * 2);
+    
+    if(random == 0){
+        img.setAttribute('src','./img/gabigol-nao.jpg');
+        resp_sim.style.display = 'block';
+        resp_nao.style.display = 'none';
+    } else{
+        img.setAttribute('src', './img/gabigol-sim.jpg');
+        resp_nao.style.display = 'block';
+        resp_sim.style.display = 'none';
+    }  
+    pergunte.value = ''
+    btn.disabled=true
+}
+
+
+
+
+
+
+
+
